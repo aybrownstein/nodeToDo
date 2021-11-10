021c6a7276eefe7f27c79b1eb39f5a84f2c46892
Aharon Brownstein <aybrownstein@gmail.com>
2021-11-10 11:12:57 -0500

    updated router

--- routes/items.js
+++ routes/items.js
@@ -6,4 +6,48 @@ router.get('/', async(req, res) => {
     res.render('items/index', { items });
 });
 
+router.get('/newitem', async(req, res) => {
+    const categories = await toDoDb.getCategories();
+    res.render('items/newitem', { categories });
+});
+
+router.post('/addItem', async(req, res) => {
+    await toDoDb.addItem(req.body);
+    res.redirect('/items/');
+});
+
+router.post('/markascomplteted', async(req, res) => {
+    await toDoDb.markCompleted(req.body);
+    res.redirect('/items/');
+});
+
+router.get('/completed', async(req, res) => {
+    const completedItems = await toDoDb.getCompletedItems();
+    res.render('items/completed', { completedItems });
+});
+
+router.get('/categories', async(req, res) => {
+    const categories = await toDoDb.getCategories();
+    res.render('items/categories', { categories });
+});
+
+router.get('/newCategory', async(req, res) => {
+    res.render('items/newcategory');
+});
+
+router.post('/addCategory', async(req, res) => {
+    await toDoDb.addCategory(req.body);
+    res.redirect('/items/');
+});
+
+router.get('/editCategory', async(req, res) => {
+    const category = await toDoDb.getcategory(req.query.id);
+    res.render('items/editcategory', { category });
+});
+
+router.post('/updateCategory', async(req, res) => {
+    await toDoDb.updateCategory(req.body);
+    res.redirect('/items/categories');
+});
+
 module.exports = router;
